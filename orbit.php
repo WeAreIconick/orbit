0aec11dd2ff232366faee13dc7414c6cf971d46d
Nick Hamze <me@nickhamze.com>
2025-09-15 12:54:18 -0500

    Fixed headers issue

--- orbit.php
+++ orbit.php
@@ -2,11 +2,14 @@
 /**
  * Plugin Name:       Orbit
  * Description:       Text that defies gravity. Words curve and glide along an invisible circular path, transforming static copy into kinetic art that draws the eye and holds attention.
- * Version:           0.1.0
- * Author:            WordPress Telex
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:      	  iconick
+ * Author URI:        https://iconick.io
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       orbit-block-wp
+ * Text Domain:       orbit
  *
  * @package Orbit
  */
@@ -15,14 +18,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
 if ( ! function_exists( 'telex_orbit_block_init' ) ) {
-	/**
-	 * Registers the block using the metadata loaded from the `block.json` file.
-	 * Behind the scenes, it registers also all assets so they can be enqueued
-	 * through the block editor in the corresponding context.
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
-	 */
 	function telex_orbit_block_init() {
 		register_block_type( __DIR__ . '/build/' );
 	}
